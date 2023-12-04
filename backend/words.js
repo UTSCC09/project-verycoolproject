a74571f4bb561a2d03ca2ef740d904bb6be4a33a
Roger Zhao <78385317+zhaoroger@users.noreply.github.com>
2023-12-04 08:23:34 -0500

    added more words

--- backend/words.js
+++ backend/words.js
@@ -1,13 +1,28 @@
 const words = [
-    "Elephant",
-    "Surfing",
-    "Disco",
-    "Astronaut",
-    "Kangaroo",
-    "Juggling",
-    "Pizza",
-    "Ballet",
-    "Firefighter",
-    "Karate"
+    "Elephant", "Surfing", "Disco", "Astronaut", "Kangaroo",
+    "Juggling", "Pizza", "Ballet", "Firefighter", "Karate",
+    "Giraffe", "Skiing", "Robot", "Pirate", "Mermaid",
+    "Sumo Wrestler", "Photographer", "Unicorn", "Saxophone", "Wrestling",
+    "Banana", "Tennis", "Squirrel", "Spacewalk", "Volleyball",
+    "Cowboy", "Butterfly", "Magician", "Ice Skating", "Yoga",
+    "Zombie", "Penguin", "Rock Climbing", "Cell Phone", "Puppet",
+    "Mime", "Hula Hoop", "Koala", "Soccer", "Kite",
+    "Swordfight", "Parrot", "Tightrope Walker", "Racecar", "Fishing",
+    "Sadness", "Jogging", "Dentist", "Sneeze", "Wizard",
+    "Traffic Jam", "Swimming", "Witch", "Cow", "Piano",
+    "Jellyfish", "Basketball", "Safari", "Cactus", "Sushi",
+    "Elephant March", "Banjo", "Tug of War", "Beehive", "Drummer",
+    "Lighthouse", "Vampire", "Clown", "Mountain Climbing", "Rowing",
+    "Zigzag", "Sitting", "Sailing", "Egg", "Dinosaur", 
+    "Singing", "Golf", "Working Out", "Hiking", "Fencing",
+    "Rainstorm", "Superhero", "Steal", "Snail", "Beyblade",
+    "Ice Cream Cone", "Football", "Sumo Wrestling", "Scuba Diving", "Telescope",
+    "Simpsons", "Ocean", "Duck", "Baseball", "Circus",
+    "Ninja", "Eagle",
+    "Lifeguard", 
+    "Drumroll", "Avalanche", 
+    "Bumblebee", "Bullying", "Zipline",
+    "Tornado", "Tired", "Skiing", "Climbing", "Sewing"
 ];
+
 export { words };
