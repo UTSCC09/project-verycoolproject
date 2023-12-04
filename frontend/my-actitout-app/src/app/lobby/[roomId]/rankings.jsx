eaed84406c579747d4b850737858df4931b82c5f
Lordpritish <60456975+Lordpritish@users.noreply.github.com>
2023-12-04 01:38:32 -0500

    Update rankings.jsx

--- frontend/my-actitout-app/src/app/lobby/[roomId]/rankings.jsx
+++ frontend/my-actitout-app/src/app/lobby/[roomId]/rankings.jsx
@@ -19,32 +19,32 @@ export default function UserRankings(props) {
             <h1 className="text-4xl font-bold mb-4">Game Over!</h1>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                {top3Players.map((player) => (
+                {top3Players.map((player,index) => (
                     <div
-                        key={player.id}
-                        className={`flex  items-center justify-between p-3 rounded-md aspect-w-1 aspect-h-1 ${player.rank === 1
+                        key={index}
+                        className={`flex  items-center justify-between p-3 rounded-md aspect-w-1 aspect-h-1 ${index === 0
                             ? 'bg-yellow-300' // Adjust the shade as needed
-                            : player.rank === 2
+                            : index === 1
                                 ? 'bg-gray-300' // Adjust the shade as needed
-                                : player.rank === 3
+                                : index === 2
                                     ? 'bg-orange-300' // Adjust the shade as needed
                                     : 'bg-gray-800 text-white'
                             }`}
                     >
                         <div className="flex flex-col">
                             <div> <Avatar seed={player.username} alt={player.id} />
-                                <div className={`text-lg font-medium mt-2 ${player.rank === 1 ? 'text-yellow-700' : 'text-gray-700'}`} style={{ textShadow: '0 0 10px rgba(255, 255, 255, 0.8), 0 0 20px rgba(255, 255, 255, 0.8)' }}>
+                                <div className={`text-lg font-medium mt-2 ${index === 0 ? 'text-yellow-700' : 'text-gray-700'}`} style={{ textShadow: '0 0 10px rgba(255, 255, 255, 0.8), 0 0 20px rgba(255, 255, 255, 0.8)' }}>
                                     {player.username}
                                 </div></div>
 
                         </div>
-                        <div className="text-lg font-bold mt-2">Rank {player.rank}</div>
+                        <div className="text-lg font-bold mt-2">Rank {index+1}</div>
                     </div>
                 ))}
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mt-8">
-                {otherPlayers.map((player) => (
+                {otherPlayers.map((player,index) => (
                     <div
                         key={player.id}
                         className={`flex  items-center justify-between p-2 rounded-md aspect-w-1 aspect-h-1 transform transition-transform duration-300 hover:scale-105`}
@@ -54,7 +54,7 @@ export default function UserRankings(props) {
                                 <div className="text-sm font-medium mt-2">{player.username}</div>
                             </div>
                         </div>
-                        <div className="text-sm mt-2">Rank {player.rank}</div>
+                        <div className="text-sm mt-2">Rank {index + 4}</div>
 
                     </div>
                 ))}
